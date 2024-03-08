c23fce9f6f166af18466a9bd57cada5d4cc1d0b4
Khang Nguyen <90955618+maxkhang@users.noreply.github.com>
2024-03-08 14:05:24 -0800

    Add files via upload

---
+++ ComplexFiles/Complex.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Complex
+{
+public:
+	double real;
+	double imag;
+public:
+	~Complex();
+	Complex();
+	Complex(const Complex&);
+	Complex(double);
+	Complex(double, double);
+	double& operator[](const char*);
+	Complex& operator=(const Complex&);
+	friend const Complex operator*(const Complex&, const Complex&);
+	friend const Complex operator/(const Complex&, const Complex&);
+	friend const Complex operator+(const Complex&, const Complex&);
+	friend const Complex operator-(const Complex&, const Complex&);
+	friend double getMagnitude(const Complex&);
+	class InputOutOfBoundsException
+	{
+	private:
+		const char* errorMessage;
+		const char* offendingIndex;
+	public:
+		InputOutOfBoundsException(const char*, const char*);
+		const char* returnError();
+		const char* returnOffendingIndex();
+	};
+};
+
+
+
