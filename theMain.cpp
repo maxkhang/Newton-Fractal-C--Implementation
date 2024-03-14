03167896397186aa95ca3b8551603c9de6bb392d
Khang Nguyen <90955618+maxkhang@users.noreply.github.com>
2024-03-14 12:07:34 -0700

    Add files via upload

--- theMain.cpp
+++ theMain.cpp
@@ -14,15 +14,7 @@ int main()
 {
 	Fractal m1(2000U, 2000U), m2, m3;
 	saveToPPM(m1, "newton1.ppm");
-	/*Complex a(6, 5);
-	Complex b(2, 2);
-	Complex c;
-	
-	c = a / b;
-	cout << getMagnitude(c) << endl;
-	*/
 	cout << "step 2" << endl;
-	
 	m2 = Fractal(m1);
 	saveToPPM(m2, "newton2.ppm");
 	cout << "step 3" << endl;
